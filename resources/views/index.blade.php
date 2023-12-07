393efd320d836fe33566035fc707fcf7d959161e
Timur Kiselchuk <timur.kiselchuk@gmail.com>
2023-12-07 09:47:15 +0200

    Changed index.blade.php to show category

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>List of Tasks</h1>
+<h1>List of Tasks</h1>
     <a href="{{ route('tasks.create') }}">Create Task</a>
 
     <ul>
         @foreach($tasks as $task)
             <li>
                 {{ $task->title }}
+                @if ($task->category)
+                    (Category: {{ $task->category->name }})
+                @endif
                 <a href="{{ route('tasks.edit', $task->id) }}">Edit</a>
                 <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
                     @csrf

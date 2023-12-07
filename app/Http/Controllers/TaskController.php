acb8a9056bf16b7088e9636124b5a78109a1613e
Timur Kiselchuk <timur.kiselchuk@gmail.com>
2023-12-07 09:45:14 +0200

    Update Task Controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -21,12 +21,26 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate();
+        // Validate the request data
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            // Add validation rules for other fields as needed
+        ]);
 
-        Task::create($request->all());
+        // Create a new task instance
+        $task = new Task();
+        $task->title = $validatedData['title'];
+        // Set other task attributes based on the form fields
+        
+        // Associate the task with the selected category
+        $category = Category::find($request->category_id);
+        $task->category()->associate($category);
 
-        return redirect()->route('tasks.index')
-                         ->with('success', 'Task created successfully');
+        // Save the task
+        $task->save();
+
+        // Redirect to a relevant route after task creation
+        return redirect()->route('tasks.index')->with('success', 'Task created successfully');
     }
 
     public function edit(Task $task)
@@ -38,12 +52,25 @@ class TaskController extends Controller
 
     public function update(Request $request, Task $task)
     {
-        // Validation can be added here using $request->validate()
+        // Validate the request data
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            // Add validation rules for other fields as needed
+        ]);
 
-        $task->update($request->all());
+        // Update the task attributes
+        $task->title = $validatedData['title'];
+        // Update other task attributes based on the form fields
+        
+        // Associate the task with the selected category
+        $category = Category::find($request->category_id);
+        $task->category()->associate($category);
 
-        return redirect()->route('tasks.index')
-                         ->with('success', 'Task updated successfully');
+        // Save the updated task
+        $task->save();
+
+        // Redirect to a relevant route after task update
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully');
     }
 
     public function destroy(Task $task)

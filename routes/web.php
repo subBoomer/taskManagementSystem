fbbdcd80e5d701cd5c0346210cb9ee86eb01af10
Timur Kiselchuk <timur.kiselchuk@gmail.com>
2023-12-07 09:25:24 +0200

    Protect Routes with Middleware

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
+    // Routes that require authentication
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
+
 Auth::routes();
 
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
